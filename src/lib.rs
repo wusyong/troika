a61d182cee75a713d7167aad972c403a45691a7f
Yu Wei Wu <wusyong9104@gmail.com>
2019-06-02 12:48:16 +0800

    Implement troika as struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
+extern crate failure;
+
 mod macros;
 pub mod troika;
+
+use std::result;
+
+pub type Result<T> = result::Result<T, failure::Error>;
--- src/troika.rs
+++ src/troika.rs
@@ -1,164 +1,200 @@
-use crate::macros::*;
+use super::macros::*;
+use crate::Result;
+use std::fmt;
 
-pub fn troika(output: &mut [Trit], input: &[Trit]) {
-    troika_var_rounds(output, input, NUM_ROUNDS);
+#[derive(Clone, Copy)]
+pub struct Troika {
+    num_rounds: usize,
+    state: [Trit; STATE_SIZE],
 }
 
-pub fn troika_var_rounds(output: &mut [Trit], input: &[Trit], num_rounds: usize) {
-    let mut state = [0u8; STATE_SIZE];
-
-    troika_absorb(&mut state, TROIKA_RATE, input, num_rounds);
-    troika_squeeze(output, TROIKA_RATE, &mut state, num_rounds);
-}
-
-fn troika_absorb(state: &mut [Trit], rate: usize, message: &[Trit], num_rounds: usize) {
-    let mut message_length = message.len();
-    let mut message_idx = 0;
-    let mut trit_idx = 0;
-
-    while message_length >= rate {
-        // Copy message block over the state
-        for trit_idx in 0..rate {
-            state[trit_idx] = message[message_idx + trit_idx];
-        }
-        troika_permutation(state, num_rounds);
-        message_length -= rate;
-        message_idx += rate;
-    }
-
-    // Pad last block
-    let mut last_block = vec![0u8; rate];
-
-    // Copy over last incomplete message block
-    for _ in 0..message_length {
-        last_block[trit_idx] = message[trit_idx];
-        trit_idx += 1;
-    }
-
-    // TODO: Check trit_idx is right here
-    // Apply padding
-    last_block[trit_idx] = PADDING;
-
-    // Insert last message block
-    for trit_idx in 0..rate {
-        state[trit_idx] = last_block[trit_idx];
-    }
-}
-
-fn troika_squeeze(hash: &mut [Trit], rate: usize, state: &mut [Trit], num_rounds: usize) {
-    let mut hash_length = hash.len();
-    let mut hash_idx = 0;
-
-    while hash_length >= rate {
-        troika_permutation(state, num_rounds);
-        // Extract rate output
-        for trit_idx in 0..rate {
-            hash[hash_idx + trit_idx] = state[trit_idx];
-        }
-        hash_idx += rate;
-        hash_length -= rate;
-    }
-
-    // Check if there is a last incomplete block
-    if hash_length % rate != 0 {
-        troika_permutation(state, num_rounds);
-        for trit_idx in 0..hash_length {
-            hash[trit_idx] = state[trit_idx];
+impl Default for Troika {
+    fn default() -> Troika {
+        Troika {
+            num_rounds: NUM_ROUNDS,
+            state: [0u8; STATE_SIZE],
         }
     }
 }
 
-fn troika_permutation(state: &mut [Trit], num_rounds: usize) {
-    assert!(num_rounds <= NUM_ROUNDS);
-
-    for round in 0..num_rounds {
-        sub_trytes(state);
-        shift_rows(state);
-        shift_lanes(state);
-        add_column_parity(state);
-        add_round_constant(state, round);
+impl fmt::Debug for Troika {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Troika: [rounds: [{}], state: {:?}",
+            self.num_rounds,
+            self.state.to_vec(),
+        )
     }
 }
 
-fn sub_trytes(state: &mut [Trit]) {
-    for sbox_idx in 0..NUM_SBOXES {
-        let sbox_input =
-            9 * state[3 * sbox_idx] + 3 * state[3 * sbox_idx + 1] + state[3 * sbox_idx + 2];
-        let mut sbox_output = SBOX_LOOKUP[sbox_input as usize];
-        state[3 * sbox_idx + 2] = sbox_output % 3;
-        sbox_output /= 3;
-        state[3 * sbox_idx + 1] = sbox_output % 3;
-        sbox_output /= 3;
-        state[3 * sbox_idx] = sbox_output % 3;
+impl Troika {
+    pub fn new(num_rounds: usize) -> Result<Troika> {
+        let mut troika = Troika::default();
+        troika.num_rounds = num_rounds;
+        Ok(troika)
     }
-}
 
-fn shift_rows(state: &mut [Trit]) {
-    let mut new_state = [0u8; STATE_SIZE];
+    pub fn state(&self) -> &[Trit] {
+        &self.state
+    }
 
-    for slice in 0..SLICES {
-        for row in 0..ROWS {
-            for col in 0..COLUMNS {
-                let old_idx = SLICESIZE * slice + COLUMNS * row + col;
-                let new_idx =
-                    SLICESIZE * slice + COLUMNS * row + (col + 3 * SHIFT_ROWS_PARAM[row]) % COLUMNS;
-                new_state[new_idx] = state[old_idx];
+    pub fn reset(&mut self) {
+        self.state = [0; STATE_SIZE];
+    }
+
+    pub fn absorb(&mut self, message: &[Trit]) {
+        let mut message_length = message.len();
+        let mut message_idx = 0;
+        let mut trit_idx = 0;
+
+        while message_length >= TROIKA_RATE {
+            // Copy message block over the state
+            for trit_idx in 0..TROIKA_RATE {
+                self.state[trit_idx] = message[message_idx + trit_idx];
+            }
+            self.permutation();
+            message_length -= TROIKA_RATE;
+            message_idx += TROIKA_RATE;
+        }
+
+        // Pad last block
+        let mut last_block = vec![0u8; TROIKA_RATE];
+
+        // Copy over last incomplete message block
+        for _ in 0..message_length {
+            last_block[trit_idx] = message[trit_idx];
+            trit_idx += 1;
+        }
+
+        // TODO: Check trit_idx is right here
+        // Apply padding
+        last_block[trit_idx] = PADDING;
+
+        // Insert last message block
+        for trit_idx in 0..TROIKA_RATE {
+            self.state[trit_idx] = last_block[trit_idx];
+        }
+    }
+
+    pub fn squeeze(&mut self, hash: &mut [Trit]) {
+        let mut hash_length = hash.len();
+        let mut hash_idx = 0;
+
+        while hash_length >= TROIKA_RATE {
+            self.permutation();
+            // Extract rate output
+            for trit_idx in 0..TROIKA_RATE {
+                hash[hash_idx + trit_idx] = self.state[trit_idx];
+            }
+            hash_idx += TROIKA_RATE;
+            hash_length -= TROIKA_RATE;
+        }
+
+        // Check if there is a last incomplete block
+        if hash_length % TROIKA_RATE != 0 {
+            self.permutation();
+            for trit_idx in 0..hash_length {
+                hash[trit_idx] = self.state[trit_idx];
             }
         }
     }
 
-    state.copy_from_slice(&new_state[..]);
-}
+    pub fn permutation(&mut self) {
+        assert!(self.num_rounds <= NUM_ROUNDS);
 
-fn shift_lanes(state: &mut [Trit]) {
-    let mut new_state = [0u8; STATE_SIZE];
-
-    for slice in 0..SLICES {
-        for row in 0..ROWS {
-            for col in 0..COLUMNS {
-                let old_idx = SLICESIZE * slice + COLUMNS * row + col;
-                let new_slice = (slice + SHIFT_LANES_PARAM[col + COLUMNS * row]) % SLICES;
-                let new_idx = SLICESIZE * (new_slice) + COLUMNS * row + col;
-                new_state[new_idx] = state[old_idx];
-            }
+        for round in 0..self.num_rounds {
+            self.sub_trytes();
+            self.shift_rows();
+            self.shift_lanes();
+            self.add_column_parity();
+            self.add_round_constant(round);
         }
     }
 
-    state.copy_from_slice(&new_state[..]);
-}
+    fn sub_trytes(&mut self) {
+        for sbox_idx in 0..NUM_SBOXES {
+            let sbox_input = 9 * self.state[3 * sbox_idx]
+                + 3 * self.state[3 * sbox_idx + 1]
+                + self.state[3 * sbox_idx + 2];
+            let mut sbox_output = SBOX_LOOKUP[sbox_input as usize];
+            self.state[3 * sbox_idx + 2] = sbox_output % 3;
+            sbox_output /= 3;
+            self.state[3 * sbox_idx + 1] = sbox_output % 3;
+            sbox_output /= 3;
+            self.state[3 * sbox_idx] = sbox_output % 3;
+        }
+    }
 
-fn add_column_parity(state: &mut [Trit]) {
-    let mut parity = [0u8; SLICES * COLUMNS];
+    fn shift_rows(&mut self) {
+        let mut new_state = [0u8; STATE_SIZE];
 
-    // First compute parity for each column
-    for slice in 0..SLICES {
-        for col in 0..COLUMNS {
-            let mut col_sum = 0;
+        for slice in 0..SLICES {
             for row in 0..ROWS {
-                col_sum += state[SLICESIZE * slice + COLUMNS * row + col];
+                for col in 0..COLUMNS {
+                    let old_idx = SLICESIZE * slice + COLUMNS * row + col;
+                    let new_idx = SLICESIZE * slice
+                        + COLUMNS * row
+                        + (col + 3 * SHIFT_ROWS_PARAM[row]) % COLUMNS;
+                    new_state[new_idx] = self.state[old_idx];
+                }
             }
-            parity[COLUMNS * slice + col] = col_sum % 3;
         }
+
+        self.state.copy_from_slice(&new_state[..]);
     }
 
-    // Add parity
-    for slice in 0..SLICES {
-        for row in 0..ROWS {
+    fn shift_lanes(&mut self) {
+        let mut new_state = [0u8; STATE_SIZE];
+
+        for slice in 0..SLICES {
+            for row in 0..ROWS {
+                for col in 0..COLUMNS {
+                    let old_idx = SLICESIZE * slice + COLUMNS * row + col;
+                    let new_slice = (slice + SHIFT_LANES_PARAM[col + COLUMNS * row]) % SLICES;
+                    let new_idx = SLICESIZE * (new_slice) + COLUMNS * row + col;
+                    new_state[new_idx] = self.state[old_idx];
+                }
+            }
+        }
+
+        self.state.copy_from_slice(&new_state[..]);
+    }
+
+    fn add_column_parity(&mut self) {
+        let mut parity = [0u8; SLICES * COLUMNS];
+
+        // First compute parity for each column
+        for slice in 0..SLICES {
             for col in 0..COLUMNS {
-                let idx = SLICESIZE * slice + COLUMNS * row + col;
-                let sum_to_add = parity[(col + 8) % 9 + COLUMNS * slice]
-                    + parity[(col + 1) % 9 + COLUMNS * ((slice + 1) % SLICES)];
-                state[idx] = (state[idx] + sum_to_add) % 3;
+                let mut col_sum = 0;
+                for row in 0..ROWS {
+                    col_sum += self.state[SLICESIZE * slice + COLUMNS * row + col];
+                }
+                parity[COLUMNS * slice + col] = col_sum % 3;
+            }
+        }
+
+        // Add parity
+        for slice in 0..SLICES {
+            for row in 0..ROWS {
+                for col in 0..COLUMNS {
+                    let idx = SLICESIZE * slice + COLUMNS * row + col;
+                    let sum_to_add = parity[(col + 8) % 9 + COLUMNS * slice]
+                        + parity[(col + 1) % 9 + COLUMNS * ((slice + 1) % SLICES)];
+                    self.state[idx] = (self.state[idx] + sum_to_add) % 3;
+                }
             }
         }
     }
-}
 
-fn add_round_constant(state: &mut [Trit], round: usize) {
-    for slice in 0..SLICES {
-        for col in 0..COLUMNS {
-            let idx = SLICESIZE * slice + col;
-            state[idx] = (state[idx] + ROUND_CONSTANTS[round][slice * COLUMNS + col]) % 3;
+    fn add_round_constant(&mut self, round: usize) {
+        for slice in 0..SLICES {
+            for col in 0..COLUMNS {
+                let idx = SLICESIZE * slice + col;
+                self.state[idx] =
+                    (self.state[idx] + ROUND_CONSTANTS[round][slice * COLUMNS + col]) % 3;
+            }
         }
     }
 }
@@ -181,12 +217,14 @@ mod test_troika {
 
     #[test]
     fn test_hash() {
-        let mut trits = [0u8; 243];
+        let mut troika = Troika::default();
+        let mut output = [0u8; 243];
         let input = [0u8; 243];
-        troika(&mut trits, &input);
+        troika.absorb(&input);
+        troika.squeeze(&mut output);
 
         assert!(
-            trits.iter().zip(HASH.iter()).all(|(a, b)| a == b),
+            output.iter().zip(HASH.iter()).all(|(a, b)| a == b),
             "Arrays are not equal"
         );
     }
