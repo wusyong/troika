c3bf9ed67f3ab8bb73e051b9bbd7c3238350b530
Yu Wei Wu <wusyong9104@gmail.com>
2019-06-30 12:53:15 +0800

    Improve benchmark

--- benches/troika_benchmark.rs
+++ benches/troika_benchmark.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 extern crate criterion;
+extern crate rand;
+use rand::{thread_rng, Rng};
 
 use criterion::Criterion;
 use troika_rust::troika::Troika;
@@ -7,8 +9,13 @@ use troika_rust::ftroika::Ftroika;
 
 fn basic_troika() {
     let mut troika = Troika::default();
-    let input = [0u8; 242];
+    let mut input = [0u8; 8019];
     let mut output = [0u8; 243];
+    let mut rng = thread_rng();
+
+    for trit in input.iter_mut() {
+        *trit = rng.gen_range(0, 3);
+    }
 
     troika.absorb(&input);
     troika.squeeze(&mut output);
@@ -16,19 +23,30 @@ fn basic_troika() {
 
 fn basic_ftroika() {
     let mut ftroika = Ftroika::default();
-    let input = [0u8; 8019];
+    let mut input = [0u8; 8019];
     let mut output = [0u8; 243];
+    let mut rng = thread_rng();
+
+    for trit in input.iter_mut() {
+        *trit = rng.gen_range(0, 3);
+    }
 
     ftroika.absorb(&input);
     ftroika.finalize();
     ftroika.squeeze(&mut output);
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("Ftroika with input of 8019 zeros", |b| {
+fn ftroika_benchmark(c: &mut Criterion) {
+    c.bench_function("Ftroika with input of 8019 trits", |b| {
         b.iter(|| basic_ftroika())
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn troika_benchmark(c: &mut Criterion) {
+    c.bench_function("Troika with input of 8019 trits", |b| {
+        b.iter(|| basic_troika())
+    });
+}
+
+criterion_group!(benches, ftroika_benchmark, troika_benchmark);
 criterion_main!(benches);
